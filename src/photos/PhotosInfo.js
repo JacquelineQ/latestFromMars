53531c21275df4864c03db9cf14dd00d293bb84d
Jacqueline <quackenbushjacqueline@gmail.com>
2023-01-07 14:43:24 -0500

    add PhotosInfo component

---
+++ src/photos/PhotosInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function PhotosInfo() {
+    return (
+        <div className="info-text-container">
+            <p className="info-text">Use this section to explore photos from all four of the NASA Mars Rovers!
+                The quickest way to get started is to click on either of the two active rover missions,
+                Curiosity and Perseverance, and click "Latest" to see the latest photos sent directly from Mars!
+                Be aware that these photos refresh daily.
+                <br/>
+                <br/>
+
+                You can also search through photos from any rover by Earth Date or Martian Sol, and further filter 
+                by camera.
+            </p>
+            </div>
+    )
+}
+
+export default PhotosInfo;
