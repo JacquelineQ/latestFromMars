348724ff5b812b35dc2b0918350319e58c2f8226
Jacqueline <quackenbushjacqueline@gmail.com>
2023-01-07 14:48:43 -0500

    add Tabs code

--- src/photos/Photos.js
+++ src/photos/Photos.js
@@ -1,19 +1,145 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import PerseveranceLatest from "./PerseveranceLatest.js";
+import PerseveranceEarth from "./PerseveranceEarth.js";
+import PerseveranceSol from "./PerseveranceSol.js";
+import CuriosityLatest from "./CuriosityLatest.js";
+import CuriosityEarth from "./CuriosityEarth.js";
+import CuriositySol from "./CuriositySol.js";
+import OpportunityEarth from "./OpportunityEarth.js";
+import OpportunitySol from "./OpportunitySol.js";
+import SpiritEarth from "./SpiritEarth.js";
+import SpiritSol from "./SpiritSol.js";
+import PhotosInfo from "./PhotosInfo.js";
+import PerseveranceInfo from "./PerseveranceInfo.js";
+import CuriosityInfo from "./CuriosityInfo.js";
+import OpportunityInfo from "./OpportunityInfo.js";
+import SpiritInfo from "./SpiritInfo.js";
 
 function Photos() {
     return (
-       <>
-       <div className="photosNav">
-            <Link to="perseverance">Perseverance</Link>
-            <Link to="curiosity">Curiosity</Link>
-            <Link to="opportunity">Opportunity</Link>
-            <Link to="spirit">Spirit</Link>
-      </div>
+        <>
+        <Tabs>
+            {/* First nav bar  */}
+            <TabList className="navBar">
+                <Tab className="tab">Info</Tab>
+                <Tab className="tab">Perseverance</Tab>
+                <Tab className="tab">Curiosity</Tab>
+                <Tab className="tab">Opportunity</Tab>
+                <Tab className="tab">Spirit</Tab>
+        </TabList>
 
-       <Outlet />
-       </>
+        <TabPanel>
+            <PhotosInfo />
+        </TabPanel>
+
+
+        {/* Perseverance nav bar  */}
+        <TabPanel>
+            <Tabs>
+                <TabList className="navBar">
+                    <Tab className="tab">Info</Tab>
+                    <Tab className="tab">Latest</Tab>
+                    <Tab className="tab">Search by Earth Date</Tab>
+                    <Tab className="tab">Search by Martian Sol</Tab>
+                </TabList>
+
+            <TabPanel>
+                <PerseveranceInfo />
+            </TabPanel>
+            <TabPanel>
+                <PerseveranceLatest />
+            </TabPanel>
+            <TabPanel>
+                <PerseveranceEarth />
+            </TabPanel>
+            <TabPanel>
+                <PerseveranceSol />
+            </TabPanel> 
+
+            </Tabs>  
+        </TabPanel>
+
+
+
+        {/* Curiosity Nav Bar  */}
+
+        <TabPanel>
+            <Tabs>
+                <TabList className="navBar">
+                    <Tab className="tab">Info</Tab>
+                    <Tab className="tab">Latest</Tab>
+                    <Tab className="tab">Search by Earth Date</Tab>
+                    <Tab className="tab">Search by Martian Sol</Tab>
+                </TabList>
+
+                <TabPanel>
+                   <CuriosityInfo />
+                </TabPanel>
+                <TabPanel>
+                    <CuriosityLatest />
+                </TabPanel>
+                <TabPanel>
+                    <CuriosityEarth />
+                </TabPanel>
+                <TabPanel>
+                    <CuriositySol />
+                </TabPanel>
+
+            </Tabs>
+        </TabPanel>
+
+        {/* Opportunity Nav Bar  */}
+
+        <TabPanel>
+            <Tabs>
+                <TabList className="navBar">
+                    <Tab className="tab">Info</Tab>
+                    <Tab className="tab">Search by Earth Date</Tab>
+                    <Tab className="tab">Search by Martian Sol</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <OpportunityInfo />
+
+                </TabPanel>
+                <TabPanel>
+                    <OpportunityEarth />
+                </TabPanel>
+                <TabPanel>
+                    <OpportunitySol />
+                </TabPanel>
+
+            </Tabs>
+        </TabPanel>
+
+        {/* Spirit Nav Bar  */}
+
+        <TabPanel>
+            <Tabs>
+                <TabList className="navBar">
+                    <Tab className="tab">Info</Tab>
+                    <Tab className="tab">Search by Earth Date</Tab>
+                    <Tab className="tab">Search by Martian Sol</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <SpiritInfo />
+                </TabPanel>
+                <TabPanel>
+                    <SpiritEarth />
+                </TabPanel>
+                <TabPanel>
+                    <SpiritSol />
+                </TabPanel>
+
+            </Tabs>
+        </TabPanel>
+
+        </Tabs>
+
+
+        </>
     )
 }
 
