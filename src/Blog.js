3ede863d8c164953dd6f7690f7008e9fa36a5e8d
Jacqueline <quackenbushjacqueline@gmail.com>
2023-01-07 13:52:38 -0500

    add blog page

---
+++ src/Blog.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Blog() {
+    return (
+        <div className="mainContainer">
+     <h1>Blog</h1>
+     <p className="mainP">Hello there! Thanks so much for visiting! This site is still currently under construction
+        while we prepare some awesome content for our fellow space nerds. Please visit the "Photos"
+        section where you can search through NASA's entire catalogue of photos taken by rovers Perseverance,
+        Curiosity, Opportunity and Spirit. Perseverance and Curiosity are updated DAILY!
+        <br />
+        <br />
+        
+        And of course check back soon for updates ;D
+     </p>
+     </div>
+    )
+}
+
+export default Blog;
